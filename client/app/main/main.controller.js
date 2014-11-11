01a79c1202ba10ef439322ad694602688b741e97
Richard Barrett-Small <rickbs@gmail.com>
2014-11-11 15:46:22 +0000

    A button and some posts

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,21 +2,15 @@
 
 angular.module('secretLanaApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.posts = [
+      {title: 'post un', upvotes: 5},
+      {title: 'post deux', upvotes: 2},
+      {title: 'post three', upvotes: 15},
+      {title: 'post quatre', upvotes: 9},
+      {title: 'post cinq', upvotes: 4}
+    ];
+    $scope.text = 'Rich';
+    $scope.addPost = function(){
+      $scope.posts.push({title: 'A new one!', upvotes: 0});
     };
   });
